ea4f662b0180e5a1e8c6820f7bf7ff514a1b2fbf
Garrett Pace <elderpace6@gmail.com>
2023-04-18 10:59:01 -0600

    try

--- addToZero.js
+++ addToZero.js
@@ -2,3 +2,14 @@
 let array = [28, 43, -12, 30, 4, 0, 12]
 
 // Write your solution below:
+
+// 1. create function
+// 2. use for-lopp to find 2 numbers
+// 3. use if-statement inside for-loop to see if the 2 numbers add to 0
+// 4. if 2 numbers add to 0, console.log(true), else console.log(false)
+
+function addToZero(){
+    for(let i=0;i<array.length;i++){
+        
+    }
+}
